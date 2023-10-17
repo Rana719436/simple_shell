14475fe4ca78f871ad977144bc8445cc45fb4096
Asmaa <eng.asmaao2017@gmail.com>
2023-10-17 15:51:14 -0700

    a

--- shell1.c
+++ shell1.c
@@ -7,55 +7,62 @@
 
 #define MAX_COMMAND_LENGTH 1024
 
-int main() {
-    char *command = NULL;
-    size_t command_len = 0;
-    ssize_t bytes_read;
-    pid_t child_pid;
+/**
+ * main - Prints a Simple shell
+ * Return: Always 0
+ */
 
-    while (1) {
-	if (isatty(STDIN_FILENO)) {
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
-        }    
-      
-        bytes_read = getline(&command, &command_len, stdin);
+int main(void)
+{
+	char *command = NULL;
+	size_t command_len = 0;
+	ssize_t bytes_read;
+	pid_t child_pid;
 
-        if (bytes_read == -1) {
-		if (isatty(STDIN_FILENO)){
-     
-            write(STDOUT_FILENO, "\n", 1);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 		}
+		bytes_read = getline(&command, &command_len, stdin);
 
-            break;
-	    free (command);
-        }
+		if (bytes_read == -1)
+		{
+			if (isatty(STDIN_FILENO))
+			{
+				write(STDOUT_FILENO, "\n", 1);
+			}
+			break;
+			free(command);
+		}
+		if (bytes_read > 0 && command[bytes_read - 1] == '\n')
+		{
+			command[bytes_read - 1] = '\0';
+		}
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("fork");
+		}
+		else if (child_pid == 0)
+		{
+			char *args[] = {NULL, NULL};
 
-
-        if (bytes_read > 0 && command[bytes_read - 1] == '\n') {
-            command[bytes_read - 1] = '\0';
-        }
-
-        child_pid = fork();
-
-        if (child_pid == -1) {
-            perror("fork");
-        } else if (child_pid == 0) {
-
-            char *args[] = {NULL, NULL};
-            args[0] = strdup(command); 
-            execve(args[0], args, NULL);
-
-            perror("./shell");
-            exit(1);
-        } else {
-
-            int status;
-            if (wait(&status) == -1) {
-                perror("wait");
-            }
-        }
-    }
-
-    free(command);
-    return 0;
+			args[0] = strdup(command);
+			execve(args[0], args, NULL);
+			perror("./shell");
+			exit(1);
+		}
+		else
+		{
+			int status;
+			if (wait(&status) == -1)
+			{
+				perror("wait");
+			}
+		}
+	}
+	free(command);
+	return 0;
 }
