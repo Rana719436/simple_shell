cfe19fcaf8c166ba3fe2481dfa2a9d7be3e697c1
Asmaa <eng.asmaao2017@gmail.com>
2023-10-18 11:46:46 -0700

    A

--- shell.h
+++ shell.h
@@ -11,8 +11,8 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_ARG_COUNT 32
 
-void printPrompt();
-char* readCommand();
+void printPrompt(void);
+char *readCommand(void);
 void executeCommand(char *command, char *args[]);
 void parseArguments(char *input, char *args[], int *arg_count);
 
